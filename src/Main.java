2381c063b518ac518f4f354a82f91225bd98a0e8
Alexander Pavlenko <pava@bk.ru>
2020-05-02 13:59:20 +0700

    Main.java modified

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        long current = 2_000_000_000;
-        long transfer = 500_000_000;
-        long result = transfer + current;
+        int current = 2_000_000_000;
+        int transfer = 500_000_000;
+        int result = transfer + current;
 
         System.out.println(result);
 
